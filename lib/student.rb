763e4064d367475bb8a11412e1cde056906e2f6f
Mitchel Parish <mitchelparish@gmail.com>
2019-02-14 22:30:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,11 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize
+  def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
     @id = id
-  end 
+  end
 
 
 
