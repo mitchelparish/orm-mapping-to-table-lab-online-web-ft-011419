33cbb8098f076910b32f01cde2e31ce33066f4f0
Mitchel Parish <mitchelparish@gmail.com>
2019-02-14 22:30:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,17 @@
 class Student
+  
+  attr_accessor :name, :grade
+  attr_reader :id 
+
+  def initialize 
+    @name = name
+    @grade = grade
+    @id = id 
+  end 
+
+
+
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
