9038c46f091892eecb99760b3b1bc3fe18a7a805
Mitchel Parish <mitchelparish@gmail.com>
2019-02-14 22:40:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,20 @@ class Student
   def self.drop_table
     sql = "DROP TABLE students;"
     DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
   end 
+
+
+
+  end
 
 
 
